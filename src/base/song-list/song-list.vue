99bd2d98fe06ad938646696cc6411e16780e9e27
zhuqiang <99299684@qq.com>
2017-08-20 22:45:42 +0800

    歌手详情页-music-list组件,滚动列表和跟随滚动的交互效果

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li class="item" v-for="song in songs">
+        <div class="content">
+          <h2 class="name">{{song.name }}</h2>
+          <p class="desc">{{ getDesc(song) }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      songs: {
+        type: Array,
+        default: []
+      }
+    },
+    data () {
+      return {}
+    },
+    methods: {
+      //   描述是歌手名和专辑名链接起来的
+      //  所以这里可以使用过滤器，也可以使用函数（这样使用还是第一次）
+      getDesc (song) {
+        return `${song.singer} . ${song.album}`
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .song-list {
+    .item {
+      display: flex
+      height 64px
+      align-items center // 垂直居中（在高度64px中）
+      box-sizing border-box
+      font-size $font-size-medium
+      .content {
+        flex 1
+        line-height 20px
+        overflow hidden
+        .name {
+          no-wrap()
+          color $color-text
+        }
+        .desc {
+          no-wrap()
+          margin-top 4px
+          color $color-text-d
+        }
+      }
+    }
+  }
+</style>
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,16 +4,32 @@
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle">
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <div class="bg-layer" ref="bgLayer"></div>
+    <scroll :data="songs" class="list" ref="list"
+            :listen-scroll="listenScroll"
+            :probe-type="probeType"
+            @scroll="listScroll"
+    >
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import SongList from 'base/song-list/song-list'
+  import Scroll from 'base/scroll/Scroll'
+
+  // 顶部留出多高的距离不能滚动
+  const RESERVED_HEIGHT = 40
+
   export default {
-    data () {
-      return {}
+    components: {
+      Scroll, SongList
     },
     props: {
       // 背景图
@@ -32,9 +48,56 @@
         default: ''
       }
     },
+    created () {
+      this.listenScroll = true  // 监听滚动事件
+      this.probeType = 3  // 滚动事件派发方式
+    },
+    mounted () {
+      // 因为这是一个scroll组件，所以要拿到el（dom元素）
+      // 为什么要动态计算高度呢？是因为背景图的高度是自适应的
+      let clientHeight = this.$refs.bgImage.clientHeight
+      this.$refs.list.$el.style.top = `${clientHeight}px`
+
+      // 记录两个值，背景图部分的高度
+      this.bgImageHeight = clientHeight
+      // 交互列表能偏移的最小高度是
+      this.minTranslateY = -(clientHeight - RESERVED_HEIGHT)
+    },
+    data () {
+      return {
+        scrollY: 0
+      }
+    },
     computed: {
       bgStyle () {
         return `background-image:url(${this.bgImage})`
+      }
+    },
+    methods: {
+      listScroll (pos) {
+        this.scrollY = pos.y
+      }
+    },
+    watch: {
+      scrollY (newY) {
+        let translateY = Math.max(this.minTranslateY, newY)
+        let zindex = 0
+        if (translateY === this.minTranslateY) {
+          let bgImageStyle = this.$refs.bgImage.style
+          bgImageStyle['padding-top'] = 0
+          bgImageStyle['height'] = `${RESERVED_HEIGHT}px`
+          zindex = 10
+          console.log(`  --- translateY:`, translateY, `newY:`, newY)
+        } else {
+          // 让背景层跟着网上移动
+          this.$refs.bgLayer.style['transform'] = `translate3d(0,${translateY}px,0)`
+          let bgImageStyle = this.$refs.bgImage.style
+          bgImageStyle['padding-top'] = `70%`
+          bgImageStyle['height'] = 0
+          zindex = 0
+          console.log(`translateY:`, translateY, `newY:`, newY)
+        }
+        this.$refs.bgImage.style['z-index'] = zindex
       }
     }
   }
@@ -92,6 +155,28 @@
         width 100%
         height 100%
         background rgba(7, 17, 27, 0.4)
+      }
+    }
+    .bg-layer {
+      position: relative
+      height: 100%
+      background: $color-background
+    }
+    .list {
+      position: fixed
+      top: 0 // 该top被js动态修改
+      bottom: 0
+      width: 100%
+      background: $color-background
+      /*overflow hidden*/ //  去掉这个是为了让列表能滚动到容器外面，实现我们的滚动挤压效果
+      .song-list-wrapper {
+        padding: 20px 30px
+      }
+      .loading-container {
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
       }
     }
   }
